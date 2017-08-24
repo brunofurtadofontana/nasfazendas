1d7dd455841228bf74593c7f6eb7772226706930
brunofurtadofontana <brunofurtado86@hotmail.com>
2017-08-24 16:51:20 -0300

    Estilizando
    index

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nas Fazendas</title>
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Bootstrap CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="row-fluid">
+		<header>
+			<div class="logo">
+				<img src="images/logo.png" width="60" />
+			</div>
+				
+	   <div class="search">
+					<input type="text" class="form-control input-sm" maxlength="64" placeholder="Olá, o que você procura?" />
+
+
+		 </div>
+				
+			<div class="user">
+				<span><a href="#"> \/ Olá Visitante</a></span>
+			</div>
+		</header>
+			<nav class="navbar navbar-inverse navbar-fixed-top" style="background:#275b2f;">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a href="">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a href="">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a href="">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a href="">HOME</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+      </ul>
+      </div>
+    </nav>
+    <div class="banner">
+      
+    </div>
+    <div class="bannerSec">
+      
+    </div>
+    <div class="anuncios">
+
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+      <div class="destaque"></div>
+
+    </div>
+
+
+	</div><!-- row-fluid -->
+
+	<!-- jQuery (necessario para os plugins Javascript do Bootstrap) -->
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
